654fd935ad1b474ff4108fd1cbd2583c1cf418d6
feekha <feeza.khan.khanzada@dnv.com>
2023-05-16 02:07:38 +0200

    first commit

--- app.py
+++ app.py
@@ -942,7 +942,7 @@ def update_excel(n_clicks):
         if 'Index' in df.columns:
             df.drop(['Index'],axis=1, inplace=True)
         #df.columns = load_org_cols()
-        df.to_excel('Projectdata.xlsx', index=False) # change this path later
+        df.to_excel('/Projectdata.xlsx', index=False) # change this path later
         n_clicks=0
         return n_clicks
     else:
